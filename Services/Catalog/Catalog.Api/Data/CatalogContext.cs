1681ddbc16a15e3b55a0585bb058d22b56d6f06b
henry.kamwamba <henry.kamwamba@angledimension.com>
2022-03-31 16:37:47 +0200

    added Context classes to the project

---
+++ Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<ProductModel> Products { get; }
+        
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+
+            Products = database.GetCollection<ProductModel>(configuration["DatabaseSettings:CollectionName"]);
+            //CatalogContextSeed.SeedData(Products);
+        }
+
+
+    }
+}
---
+++ Services/Catalog/Catalog.Api/Data/CatalogContextSeed.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<ProductModel> productCollection) 
+        {
+            bool productExists = productCollection.Find(p => true).Any();
+            if (!productExists)
+            {
+
+            }
+        }
+    }
+}
