d3ef39280675f926821230871c40baadcacfd1d4
henry.kamwamba <henry.kamwamba@angledimension.com>
2022-03-31 20:58:05 +0200

    Added ProductRepository and interface

---
+++ Services/Catalog/Catalog.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Api.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductModel>> GetProducts();
+        Task<ProductModel> GetProduct(string id);
+        Task<IEnumerable<ProductModel>> GetProductByName(string name);
+        Task<IEnumerable<ProductModel>> GetProductByCategory(string categoryName);
+
+        Task CreateProduct(ProductModel product);
+        Task<bool> UpdateProduct(ProductModel product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
